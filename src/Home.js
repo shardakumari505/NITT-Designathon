9b1bf255bdf431788d455361711b79ee1f2dfac0
Sharda Kumari <shardakumari505@gmail.com>
2022-03-05 10:54:11 +0530

    First commit

---
+++ src/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Home.css';
+
+const Home = () =>{
+    return(<div className='homepage'>
+            <div className='hometopcard'>
+                <div className='hometopellipse'></div>
+            </div>
+        </div>)
+}
+
+export default Home;
